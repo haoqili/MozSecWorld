4c27069ff31a4d9971d650543352c8c5330be234
HaoQi Li <haoqili@mit.edu>
2011-06-08 11:38:54 -0700

    fixed typo, template variable name works :D

--- msw/views.py
+++ msw/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 def index(request):
     # return HttpResponse("Hello, world. This is going to be where the Mozilla Secure World is going to be!")
 
-    rendered = render_to_response('mswTemplates/index.html', {'title_chunk' : 'barbar'})
+    rendered = render_to_response('mswTemplates/index.html', {"title_chunk" : "barbari"})
     return rendered
 
 
