229730b9446dd1b54b5299f83a327370ee06b9a3
HaoQi Li <haoqili@mit.edu>
2011-06-08 11:19:41 -0700

    smaller change, impler than last time

--- msw/views.py
+++ msw/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import render_to_response, get_object_or_404
 def index(request):
     # return HttpResponse("Hello, world. This is going to be where the Mozilla Secure World is going to be!")
 
-    renderString = render_to_string('mswTemplates/index.html', {'title_chunk' : 'barbar'})
-    return HttpResponse( renderString )
+    rendered = render_to_response('mswTemplates/index.html', {'title_chunk' : 'barbar'})
+    return rendered
 
 
