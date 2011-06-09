b0f9ff3b94495135c3bc599da690e64d18964de8
HaoQi Li <haoqili@mit.edu>
2011-06-09 16:10:52 -0700

    added msw to admin

---
+++ apps/msw/admin.py
@@ -0,0 +1,8 @@
+from msw.models import Page
+from django.contrib import admin
+
+# could add more complicated stuff here consult:
+#    tutorial: https://docs.djangoproject.com/en/dev/intro/tutorial02/#enter-the-admin-site
+#    tutorial finished admin.py: https://github.com/haoqili/Django-Tutorial-Directory/blob/master/tutorialSite/polls/admin.py
+
+admin.site.register(Page)
