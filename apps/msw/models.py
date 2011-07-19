2aa10a0d9607413952b03ffc9581c88c54a3c263
HaoQi Li <haoqili@mit.edu>
2011-07-18 22:57:41 -0700

    :( #19 'richtext' is still undefined

--- apps/msw/models.py
+++ apps/msw/models.py
@@ -9,6 +9,7 @@ from django.db import models
 from django.forms import ModelForm
 from django.template.defaultfilters import slugify
 from django.conf import settings
+from commons.urlresolvers import reverse
 # User Profile / Authentication
 from django.contrib.auth.models import User
 
